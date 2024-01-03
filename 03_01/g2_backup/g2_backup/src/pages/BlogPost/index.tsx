fb401620fa45b418c3ec649bfe8a9f117681fd26
NataliaPienio <90185181+NataliaPienio@users.noreply.github.com>
2024-01-03 08:44:27 +0100

    Add files via upload

---
+++ 03_01/g2_backup/g2_backup/src/pages/BlogPost/index.tsx
@@ -0,0 +1,28 @@
+import {useParams} from "react-router-dom"
+import {Post} from "../../types";
+import {useEffect, useState} from "react";
+
+function BlogPost(){
+    const {id} = useParams<{id?: string}>() || {}
+
+    const [post, setPost] = useState<Post | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<boolean>(false)
+
+    useEffect(()=> {
+        setLoading(true)
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+            .then((post: Post) =>{
+                setPost(post)
+            })
+            .catch(() => setError(true))
+            .finally(() => setLoading(false))
+    }, [])
+
+    return (
+        <div></div>
+    )
+}
+
+export default BlogPost
